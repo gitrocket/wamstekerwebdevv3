2389f2acc4059f3e37c3e791cf40fcf554282a7d
gitrocket <emilewamsteker@gmail.com>
2020-12-17 08:41:12 -0800

    initial commit

---
+++ js/script.js
@@ -0,0 +1,33 @@
+
+
+
+
+// this function sets the active class for the menu
+function myFunction(e) {
+  var elems = document.querySelectorAll(".active");
+  [].forEach.call(elems, function(el) {
+    el.classList.remove("active");
+  });
+  e.target.className = "active";
+}
+
+// source of active class
+// https://jsfiddle.net/uu152uu9/
+
+// smooth scrolling with vanilla JS
+const links = document.querySelectorAll(".navbar ul a");
+ 
+for (const link of links) {
+  link.addEventListener("click", clickHandler);
+}
+ 
+function clickHandler(e) {
+  e.preventDefault();
+  const href = this.getAttribute("href");
+  const offsetTop = document.querySelector(href).offsetTop;
+ 
+  scroll({
+    top: offsetTop,
+    behavior: "smooth"
+  });
+}
